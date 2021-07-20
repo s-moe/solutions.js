1950aac261c2bfff31d391df07b62ddf11aa7e80
s-moe <sarahannmoe@gmail.com>
2021-07-20 16:39:58 -0700

    palindrome completed

--- solutions.js
+++ solutions.js
@@ -3,5 +3,16 @@
 // An argument is the specific data that is passed when the function is called.
 
 //2. Within a function, what is the difference between return and console.log?
-// Console.log logs the data or output of something to the console which isn't necessarily visible on the browser. 
+// Console.log logs the data or output of something to the console which isn't necessarily visible on the browser.
 // Return causes the outcome of the function to be rendered to the browser or screen.
+
+//3. Palindrome
+
+const checkPalindrome = (string) => {
+  const word = string.toLowerCase();
+  const reverseStr = word.split("").reverse().join("");
+  return word === reverseStr;
+
+};
+
+console.log(checkPalindrome("Eye"))
